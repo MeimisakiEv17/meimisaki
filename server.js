f0841192874e511494696200cea914fa6411969b
MeimisakiEv17 <6273mapd@gmail.com>
2025-02-27 06:57:11 +0900

    承認変更

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 // ✅ MongoDB に接続
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
